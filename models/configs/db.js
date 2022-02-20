80931504a0063554d426028da339a837cf9a4474
ABDOURAHMANE SOUMAILA <ibrahim.abdourahmanesoumaila@cgi.com>
2022-02-20 09:24:21 +0100

    update

--- models/configs/db.js
+++ models/configs/db.js
@@ -1,12 +1,18 @@
 const mongoose = require("mongoose");
-
+const dbUrlLocal = "mongodb://localhost:27017/KasuaAppDB";
+const dbUrl =
+    "mongodb+srv://IB:p0hGpkOV4Lml8NoM@cluster0.9cper.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
 mongoose.connect(
-    "mongodb://localhost:27017/KasuaAppDB", {
+    dbUrl, {
         useNewUrlParser: true,
-        useUnifiedTopology: true
+        useUnifiedTopology: true,
     },
     (err) => {
-        if (!err) console.log("[kasuwa-app-api]---- connection to KasuaAppDB : success");
-        else console.log("[kasuwa-app-api]---- connection to KasuaAppDB : not success" + err);
+        if (!err)
+            console.log("[kasuwa-app-api]---- connection to KasuaAppDB : success");
+        else
+            console.log(
+                "[kasuwa-app-api]---- connection to KasuaAppDB : not success " + err
+            );
     }
-)
+);
