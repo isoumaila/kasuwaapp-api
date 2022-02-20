3d0af14b632b0b8755fe7901074494ce0b71da3f
ABDOURAHMANE SOUMAILA <ibrahim.abdourahmanesoumaila@cgi.com>
2022-02-20 09:53:24 +0100

    update

--- models/configs/db.js
+++ models/configs/db.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 const dbUrlLocal = "mongodb://localhost:27017/KasuaAppDB";
 const dbUrl =
-    "mongodb+srv://IB:p0hGpkOV4Lml8NoM@cluster0.9cper.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+    "mongodb+srv://IB:p0hGpkOV4Lml8NoM@cluster0.9cper.mongodb.net/KasuaAppDB?retryWrites=true&w=majority";
 mongoose.connect(
     dbUrl, {
         useNewUrlParser: true,
